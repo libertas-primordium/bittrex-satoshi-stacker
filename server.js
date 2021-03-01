3d576831b20817218c45b6169cfeeada0f42840a
libertas-primordium <tylerdurdensfuckingkhakis@protonmail.com>
2021-02-28 18:29:57 -0800

    further on outline

--- server.js
+++ server.js
@@ -26,20 +26,33 @@ const signature = CryptoJS.HmacSHA512(preSign, apiSecret).toString(CryptoJS.enc.
         //TO-DO: post order
         //TO-DO: cancel order
 
-    //TO-DO: WEBSOCKET API
-        //TO-DO: WEBSOCKET_API authentication function
-        //TO-DO: get price data
-        //TO-DO: connection alive test
+    //TO-DO: Interract with WEBSOCKET API websocket.js
+        //TO-DO: 
         
     
-    
+    //TO-DO: REST-API endboints for 3 exchanges
+        //TO-DO: CoinbasePRO
+            //TO-DO: collect 2x 15min candles and combine intro array of 30min periods
+        //TO-DO: Bitstamp
+        //TO-DO: Bitfinex
     
 
 //TO-DO: build logic
+    //TO-DO: Combine data from 3 exchanges into single array of volume-weighted averaged data
     //TO-DO: WMA
     //TO-DO: Weighted Directional Bias using v2.0
     //TO-DO: BUY/SELL trigers based on inputs from WDB
         //TO-DO: object array for lookback period
             //TO-DO: contains WBD value and a boolean for growing/shrinking
         //TO-DO: eval BUY/SELL conditions
-        //TO-DO: calculate order sizing based on inputs from WDB
+            //TO-DO: 
+            /* 
+            if WDB is >0.618 or <-0.382
+            && if last period was shrinking
+            && if period before last was growing
+            && if >=7 of 9 lookback starting from arr[1] are growing
+                then trigger buy or sell
+            */
+        //TO-DO: calculate order sizing based on inputs from WDB (and price arbitrage against index)?
+            //TO-DO: minimumSize * WDB/threshold
+            //TO-DO: check balance, if available > order size, send order
